cb0a545e7b2527b6bd1cd401655f1a5fd4e853d6
Paul Kermann <paulkermann@tutanota.com>
2022-09-29 11:20:18 +0300

    lazy

--- cle/backends/lazy.py
+++ cle/backends/lazy.py
@@ -11,28 +11,22 @@ class NonEmptySegment(EmptySegment):
         return False
 
 class LazyClemory(Clemory):
-    def __init__(self, owner: 'LazyBackend', chunk_size=2**12, max_resident=2**24):
+    def __init__(self, owner: 'LazyBackend', chunk_size=0x1000):
         """
         chunk_size must be a power of two
         """
         self.owner = owner
-        self.lru_order = OrderedDict()
         self.chunk_size = chunk_size
-        self.max_resident = max_resident
         self.resident = 0
 
         super().__init__(self.owner.arch)
 
         self.min_addr = self.owner.min_addr
         self.max_addr = self.owner.max_addr
-        self.consecutive = False
+        self.loaded_pages = []
 
     def _update_min_max(self):
         pass
-
-    @property
-    def _real_max_resident(self):
-        return self.max_resident + self.chunk_size * 2
 
     def __getitem__(self, k):
         return self.load(k, 1)[0]
@@ -72,15 +66,9 @@ class LazyClemory(Clemory):
 
         return missing
 
-    def _evict_one(self):
-        first_key = next(iter(self.lru_order.keys()), None)
-        if first_key is None:
-            raise Exception("Something has gone horribly wrong")
-
     def remove_backer(self, start):
         popped = super().remove_backer(start)
         self.resident -= len(popped)
-        self.lru_order.pop(start)
         return popped
 
     def add_backer(self, start, data, overwrite=False):
@@ -99,55 +87,28 @@ class LazyClemory(Clemory):
             data = bytearray(data)
         bisect.insort(self._backers, (start, data))
         self.resident += len(data)
-        self.lru_order[start] = None
 
     def next_region(self, addr):
         return self.owner.segments.find_region_next_to(addr).min_addr
 
     def backers(self, addr=0):
-        while addr < self.max_addr:
-            chunk_addr = addr & ~(self.chunk_size - 1)
-            self.make_resident(chunk_addr, self.chunk_size)
-            end = chunk_addr + self.chunk_size
-            for start, backer in super().backers(addr):
-                if start > chunk_addr + self.chunk_size:
-                    break
-                yield start, backer
-                end = max(end, start + len(backer))
-            addr = end
+        if not self.owner.segments.find_region_containing(addr):
+            return
+
+        self.make_resident(addr, self.chunk_size)
+        for start, backer in super().backers(addr):
+            yield start, backer
 
     def make_resident(self, addr, size):
-        if size > self.max_resident:
-            raise MemoryError("Cannot hold %#x bytes in memory at once. lol" % size)
-        while size > 0:
-            region_start, region_size = self.region_containing(addr)
-            if region_start is None:
-                new_addr = self.next_region(addr)
-                size -= new_addr - addr
-                addr = new_addr
+        addr_alligned_down = addr &~(self.chunk_size - 1)
+        size_alligned_up = (size + (self.chunk_size - 1)) &~(self.chunk_size - 1)
 
-            resident_start = max(region_start, addr & ~(self.chunk_size - 1))
-            resident_end = min(((addr + size - 1) & ~(self.chunk_size - 1)) + self.chunk_size, region_start + region_size)
+        for i in range(addr_alligned_down, addr_alligned_down + size_alligned_up, self.chunk_size):
+            if i in self.loaded_pages:
+                continue
 
-            # annoying fixed-point algorithm. imagine [0, 0x1001] is mapped and you request [0x1000, 0x3000]
-            # and the limit is 0x2000. You gotta evict the first mapping.
-            while True:
-                splitted = self._split_to_unmapped(resident_start, resident_end)
-                if not splitted:
-                    break
-                for real_start, real_end in splitted:
-                    while real_end - real_start + self.resident > self._real_max_resident:
-                        self._evict_one()
-                    self.add_backer(real_start, self.owner._load_data(real_start, real_end - real_start))
-
-            # ensure the stuff that was already resident doesn't get evicted next round
-            for start, backer in super().backers(resident_start):
-                if start >= resident_end:
-                    break
-                self.lru_order.move_to_end(start)
-
-            size = addr + size - resident_end
-            addr = resident_end
+            self.loaded_pages.append(i)
+            self.add_backer(i, self.owner._load_data(i, self.chunk_size))
 
     def load(self, addr, size):
         self.make_resident(addr, size)
@@ -164,8 +125,8 @@ class LazyBackend(Backend):
     """
     This is a set of tools which should help you write a backend which accesses some memory space lazily.
     """
-    def __init__(self, binary, arch, entry_point, **kwargs):
-        super().__init__(binary, None, arch=arch, entry_point=entry_point, **kwargs)
+    def __init__(self, binary, arch, **kwargs):
+        super().__init__(binary, None, arch=arch, **kwargs)
 
         self._memory_map = None
         self._segments = None
@@ -176,18 +137,14 @@ class LazyBackend(Backend):
     @property
     def segments(self) -> Regions:
         if self._segments is None:
-            self._load_memory_map()
+            memory_map = self._load_memory_map()
+            self._segments = Regions([NonEmptySegment(addr, size) for addr, size in memory_map])
         return self._segments
 
     def _load_memory_map(self):
-        """
-        Load the memory map and retain it in self._segments. By default, it will just assume the low half of the address
-        space is flat-mapped and rwx.
-        """
-        memory_map = [(0, 2**(self.arch.bits - 1))]
-        self._segments = Regions([NonEmptySegment(addr, size) for addr, size in memory_map])
-
-    CLEMORY_CLASS = LazyClemory
+        raise NotImplementedError
 
     def _load_data(self, addr: int, size: int):
         raise NotImplementedError
+
+    CLEMORY_CLASS = LazyClemory
---
+++ test_lazy.py
@@ -0,0 +1,48 @@
+import cle
+import cle.backends
+from cle.backends import Backend, register_backend
+from cle.memory import Clemory
+
+import angr, archinfo
+from io import BytesIO
+import cle.backends.lazy
+import binascii
+import logging
+import pdb
+
+logging.getLogger().setLevel(-50)
+
+class lazy_amd64_backend(cle.backends.lazy.LazyBackend):
+    def __init__(self, binary, binary_stream, **kwargs):
+        super().__init__(binary, archinfo.arch_amd64.ArchAMD64(), **kwargs)
+
+    def _load_data(self, addr, size):
+        if addr == 0x111119000:
+            cur_data = binascii.unhexlify("48C7C037130000C3")
+            return cur_data + (size - len(cur_data)) * b"\x00"
+
+        return b"\x01" * size
+
+    def _load_memory_map(self):
+        lower_half = (0, (1<<47) - 1)
+        upper_half_start = ((1<<47) - 1) ^ 0xffffffffffffffff
+        upper_half_size = ((1 << 64) - upper_half_start)
+        
+        got =  [lower_half, (upper_half_start, upper_half_size)]
+        return got
+
+register_backend("lazy", lazy_amd64_backend)
+
+def main():
+    stream = BytesIO(b"\x00" * 0)
+    proj = angr.Project(stream, main_opts={"backend": "lazy", "entry_point": 0})
+    state = proj.factory.call_state(0x111119000, stack_base=0x111110000)
+    sm = proj.factory.simulation_manager(state)
+    sm.explore()
+
+    state = sm.deadended[0]
+    assert state.regs.rax.concrete
+    assert state.solver.eval(state.regs.rax) == 0x1337
+
+if __name__ == "__main__":
+    main()
